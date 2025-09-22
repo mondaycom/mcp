754073a5181166c35009924777b033471e8c1826
Rob Paulsen <robertpau@monday.com>
2025-09-22 10:26:34 +0100

    Simple get items page tool

--- packages/agent-toolkit/src/monday-graphql/queries.graphql.ts
+++ packages/agent-toolkit/src/monday-graphql/queries.graphql.ts
@@ -527,6 +527,29 @@ export const exportMarkdownFromDoc = gql`
   }
 `;
 
+export const getBoardItemsPage = gql`
+  query GetBoardItemsPage($boardId: ID!, $limit: Int, $cursor: String, $includeColumns: Boolean!) {
+    boards(ids: [$boardId]) {
+      id
+      name
+      items_page(limit: $limit, cursor: $cursor) {
+        items {
+          id
+          name
+          created_at
+          updated_at
+          column_values @include(if: $includeColumns) {
+            id
+            text
+            value
+          }
+        }
+        cursor
+      }
+    }
+  }
+`;
+
 export const getWorkspaceInfo = gql`
   query getWorkspaceInfo($workspace_id: ID!) {
     workspaces(ids: [$workspace_id]) {
