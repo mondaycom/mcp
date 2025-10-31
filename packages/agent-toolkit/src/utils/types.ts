7e5782e9fb08114fc20cde5120291423a9e75b3c
Damian Rakus <damianra@monday.com>
2025-10-31 12:34:25 +0100

    small fix to type declaration

--- packages/agent-toolkit/src/utils/types.ts
+++ packages/agent-toolkit/src/utils/types.ts
@@ -2,4 +2,4 @@ import { ColumnType } from "src/monday-graphql";
 
 export const NonDeprecatedColumnType = Object.fromEntries(
   Object.entries(ColumnType).filter(([key]) => key !== "Person")
-) as Record<Exclude<keyof typeof ColumnType, "Person">, string>;
+) as Record<Exclude<keyof typeof ColumnType, "Person">, ColumnType>;
