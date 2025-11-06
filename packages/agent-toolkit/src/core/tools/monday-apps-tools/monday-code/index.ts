8bccd3e86eb423f89496fd701ab2fe575d7695d8
Greg Rashkevitch <gregr@monday.com>
2025-11-06 20:44:20 +0200

    update app feature types + remove redundant apps routes

--- packages/agent-toolkit/src/core/tools/monday-apps-tools/monday-code/index.ts
+++ packages/agent-toolkit/src/core/tools/monday-apps-tools/monday-code/index.ts
@@ -1,16 +1,13 @@
 import { GetDeploymentStatusTool } from './get-deployment-status';
-import { GetTunnelTokenTool } from './get-tunnel-token';
 import { SetEnvironmentVariableTool } from './set-environment-variable';
 import { ListEnvironmentVariableKeysTool } from './list-environment-variable-keys';
 
 export const codeTools = [
   GetDeploymentStatusTool,
-  GetTunnelTokenTool,
   SetEnvironmentVariableTool,
   ListEnvironmentVariableKeysTool,
 ];
 
 export * from './get-deployment-status';
-export * from './get-tunnel-token';
 export * from './set-environment-variable';
 export * from './list-environment-variable-keys';
