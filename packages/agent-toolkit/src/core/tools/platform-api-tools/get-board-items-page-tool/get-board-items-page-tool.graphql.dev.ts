15c8e1e7a119f3ca581be244be2a4225e732f48f
Damian Rakus <damianra@monday.com>
2026-03-06 17:03:19 +0100

    Pull __typename

--- packages/agent-toolkit/src/core/tools/platform-api-tools/get-board-items-page-tool/get-board-items-page-tool.graphql.dev.ts
+++ packages/agent-toolkit/src/core/tools/platform-api-tools/get-board-items-page-tool/get-board-items-page-tool.graphql.dev.ts
@@ -3,6 +3,7 @@ import { gql } from 'graphql-request';
 export const searchItemsV2Dev = gql`
   query SearchItemsV2Dev($query: String!, $limit: Int!, $filters: SearchFiltersInput!) {
     search_v2(query: $query, limit: $limit, filters: $filters) {
+      __typename
       ... on ItemSearchResult {
         data {
           id
